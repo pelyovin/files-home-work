af98b5ca22dfc3624459212732cc49171ad0f582
Alexander Pelyovin <pelyovin.sanya@yandex.ru>
2024-07-15 14:23:54 +0300

    Task 2

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 # Задача 1
 with open('recipes.txt') as f:
     cook_book = {}
@@ -12,3 +10,21 @@ with open('recipes.txt') as f:
             ingredients.append({'ingredient_name': name, 'quantity': int(quantity), 'measure': measure})
         f.readline()
         cook_book[dish_name] = ingredients
+
+
+# Задача 2
+def get_shop_list_by_dishes(dishes, person_count):
+    """Функция, принимает на вход список блюд из cook_book и количество персон для кого мы будем готовить
+    и возвращает словарь с названием ингредиентов и его количества для блюда"""
+    result = {}
+    for dish in dishes:
+        for ingredient in cook_book[dish]:
+            if ingredient['ingredient_name'] not in result:
+                result[ingredient['ingredient_name']] = {'measure': ingredient['measure'],
+                                                         'quantity': ingredient['quantity'] * person_count}
+            else:
+                result[ingredient['ingredient_name']]['quantity'] *= 2
+    return result
+
+
+print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
